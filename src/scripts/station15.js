b4af48b6ea5bbe130cbfb47fb6e7cdf5c95dd1db
Loukas <davidsizu5051r2@gmail.com>
2024-11-16 23:57:36 +0900

    station15終わった_2024-11-16

---
+++ src/scripts/station15.js
@@ -0,0 +1,11 @@
+function displayList(){
+    const fruit = document.getElementById("fruits");
+
+    fruit.innerHTML =`
+        <ul>
+            <li>リンゴ</li>
+            <li>イチゴ</li>
+            <li>ブドウ</li>
+        </ul>
+    `;
+}
